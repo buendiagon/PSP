d05e2d494655cd2fd116b1ea3e3d71f69fdbc144
unknown <BIENESTAR APRENDIZ@SJGDFPRGV8G120.SENA.RED>
2018-08-31 22:59:18 -0500

    s

--- app/src/main/java/com/example/bienestaraprendiz/psp/tiempo/ConvertirTiempo.java
+++ app/src/main/java/com/example/bienestaraprendiz/psp/tiempo/ConvertirTiempo.java
@@ -20,8 +20,22 @@ public class ConvertirTiempo {
         tiempo2=dateFormat.format(date2).split(":");
         int horasMillis1=Integer.valueOf(tiempo1[0])*60*60000;
         int minutosMillis1=Integer.valueOf(tiempo1[1])*60000;
+        int segMillis1=Integer.valueOf(tiempo1[2])*1000;
+        int horasMillis2=Integer.valueOf(tiempo2[0])*60*60000;
+        int minutosMillis2=Integer.valueOf(tiempo2[1])*60000;
+        int segMillis2=Integer.valueOf(tiempo2[2])*1000;
+        int millis1=horasMillis1+minutosMillis1+segMillis1;
+        int millis2=horasMillis2+minutosMillis2+segMillis2;
+        int resultado=millis2-millis1;
 
+        int convertHoras=((resultado/(60*60000))%24);
+        int convertMinutos=((resultado/60000)%60);
+        int convertSeg=((resultado/1000)%60);
 
-        return "";
+        String mostrarHoras=String.format("%02d",convertHoras);
+        String mostrarMinutos=String.format("%02d",convertMinutos);
+        String mostrarSegundos=String.format("%02d",convertSeg);
+
+        return mostrarHoras+":"+mostrarMinutos+":"+mostrarSegundos;
     }
 }
