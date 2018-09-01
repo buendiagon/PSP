2d2ddd248bb2b7d01011fb4f33d5603eef9dba8d
unknown <BIENESTAR APRENDIZ@SJGDFPRGV8G120.SENA.RED>
2018-08-31 23:26:55 -0500

    d

--- app/src/main/java/com/example/bienestaraprendiz/psp/MainActivity.java
+++ app/src/main/java/com/example/bienestaraprendiz/psp/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.bienestaraprendiz.psp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.bienestaraprendiz.psp.adaptadores.AdapterProyectos;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> proyectos;
+    RecyclerView recycler;
+    AdapterProyectos adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        proyectos=new ArrayList<>();
+
+        for(int i=0;i<60;i++){
+            proyectos.add("#"+i);
+        }
+
+
+        recycler=findViewById(R.id.contenedor);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        adapter=new AdapterProyectos(proyectos);
+        recycler.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/bienestaraprendiz/psp/adaptadores/AdapterProyectos.java
+++ app/src/main/java/com/example/bienestaraprendiz/psp/adaptadores/AdapterProyectos.java
@@ -2,30 +2,58 @@ package com.example.bienestaraprendiz.psp.adaptadores;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class AdapterProyectos extends RecyclerView.Adapter<AdapterProyectos.listaViewHolder> {
+import com.example.bienestaraprendiz.psp.R;
+
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+public class AdapterProyectos extends RecyclerView.Adapter<AdapterProyectos.listaViewHolder> implements View.OnClickListener{
+    ArrayList<String> proyectos;
+    private View.OnClickListener listener;
+
+    public AdapterProyectos(ArrayList<String> proyectos) {
+        this.proyectos = proyectos;
+    }
 
     @NonNull
     @Override
     public AdapterProyectos.listaViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_list,null,false);
+        return new listaViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull AdapterProyectos.listaViewHolder listaViewHolder, int i) {
+        listaViewHolder.project.setText(proyectos.get(i));
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return proyectos.size();
     }
-    class listaViewHolder extends RecyclerView.ViewHolder {
 
+    public void setOnClickListener(View.OnClickListener listener){
+        this.listener=listener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(listener!=null){
+            listener.onClick(view);
+        }
+    }
+
+    class listaViewHolder extends RecyclerView.ViewHolder {
+        TextView project;
         public listaViewHolder(@NonNull View itemView) {
             super(itemView);
+            project=itemView.findViewById(R.id.project);
         }
     }
 }
