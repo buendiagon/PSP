8083885b9896440c05396d257a40d03b08415159
unknown <BIENESTAR APRENDIZ@SJGDFPRGV8G120.SENA.RED>
2018-08-31 23:04:54 -0500

    d

--- app/src/main/java/com/example/bienestaraprendiz/psp/adaptadores/AdapterProyectos.java
+++ app/src/main/java/com/example/bienestaraprendiz/psp/adaptadores/AdapterProyectos.java
@@ -1,4 +1,31 @@
 package com.example.bienestaraprendiz.psp.adaptadores;
 
-public class AdapterProyectos extends  {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AdapterProyectos extends RecyclerView.Adapter<AdapterProyectos.listaViewHolder> {
+
+    @NonNull
+    @Override
+    public AdapterProyectos.listaViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterProyectos.listaViewHolder listaViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    class listaViewHolder extends RecyclerView.ViewHolder {
+
+        public listaViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
